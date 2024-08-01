fce8611f4d5100368c4476d8ff42c228a979200a
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-08-01 10:52:30 +0000

    Add helpers for setting errors and writing -1

--- src/shims/unix/linux/foreign_items.rs
+++ src/shims/unix/linux/foreign_items.rs
@@ -142,7 +142,7 @@ pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
                         let _flags = this.read_scalar(&args[3])?.to_i32();
 
                         this.gen_random(ptr, len)?;
-                        this.write_scalar(Scalar::from_target_usize(len, this), dest)?;
+                        this.write_int(len, dest)?;
                         Ok(EmulateItemResult::NeedsReturn)
                     }
                     // `futex` is used by some synchronization primitives.
